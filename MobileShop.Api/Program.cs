cdb3a9e36b3c9b7b55d9590442be823107acd667
shosuz-evangelist <77768717+shosuz-evangelist@users.noreply.github.com>
2026-02-01 07:42:19 +0000

    Add CORS configuration for mobile apps

--- MobileShop.Api/Program.cs
+++ MobileShop.Api/Program.cs
@@ -60,6 +60,17 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// CORS 設定を追加
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyMethod()
+              .AllowAnyHeader();
+    });
+});
+
 // Add DbContext
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseNpgsql(connectionString));
@@ -111,6 +122,7 @@ using (var scope = app.Services.CreateScope())
 app.UseSwagger();
 app.UseSwaggerUI();
 
+app.UseCors("AllowAll");
 app.UseAuthorization();
 app.MapControllers();
 
